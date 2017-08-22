f9aa87a310704855cd3f049144635b9d4733304e
Vladimir Grishkin <vpgrishkin@gmail.com>
2017-08-22 14:23:05 +0300

    local work - done

--- README.md
+++ README.md
@@ -1,6 +1,60 @@
 # Shop Score Page
 
-{TODO. There will be project description}
+The program allows you to monitor online store KPI. The main indicator: waiting time for processing requests.  There are the status colors:
+1. Green - the waiting time is less than 7 minutes.
+2. Yellow - waiting time from 7 to 30 minutes.
+3. Red - waiting time is more than 30 minutes.
+
+Additional indicators:
+- number of unprocessed orders;
+- number of orders processed for the current day.
+
+The information is refreshed every 10 minutes.
+
+# How to use
+
+## The example on Heroku
+
+[You can see the example on Heroku](https://todo.herokuapp.com/)
+
+## Install virtual envaroment and requirements
+
+``` #!bash
+
+virtualenv -p python3 env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+## Set the database connection parameter
+
+### Unix
+``` #!bash
+
+DB_CONFIG = 'host={host} dbname={dbname} port={port} user={user} password={password}'
+```
+
+You should fill in:
+- `{host}` – database host address (defaults to UNIX socket if not provided);
+- `{dbname}` – the database name (database is a deprecated alias);
+- `{port}` – connection port number (defaults to 5432 if not provided); 
+- `{user}` – a user name;
+- `{password}` – a password.
+
+### Windows
+``` #!bash
+
+SET DB_CONFIG = 'host={host} dbname={dbname} port={port} user={user} password={password}'
+```
+
+## Run local sever
+
+``` #!bash
+
+python server.py
+```
+
+Open [http://127.0.0.1:5000/](http://127.0.0.1:5000/)
 
 # Project Goals
 
--- server.py
+++ server.py
@@ -1,10 +1,86 @@
-from flask import Flask, render_template
+import datetime
+import os
+
+from flask import Flask, render_template, jsonify, send_from_directory, request
+import psycopg2
+
 
 app = Flask(__name__)
 
+DB_CONFIG = os.getenv("DB_CONFIG")
+DATA_REFRESH_INTERVAL = 10 * 1000
+RED_INTERVAL = 30
+YELLOW_INTERVAL = 7
+
+
 @app.route('/')
 def score():
-    return render_template('score.html')
+    return render_template('score.html', DATA_REFRESH_INTERVAL=DATA_REFRESH_INTERVAL, RED_INTERVAL=RED_INTERVAL, YELLOW_INTERVAL=YELLOW_INTERVAL)
+
+
+@app.route('/robots.txt')
+def fetch_robots_txt():
+    return send_from_directory(app.static_folder, request.path[1:])
+
+
+@app.route('/style.css')
+def fetch_style_css():
+    return send_from_directory(app.static_folder, request.path[1:])
+
+
+@app.route('/kpi.json')
+def fetch_kpi_json():
+    with psycopg2.connect("host=shopscore.devman.org dbname=shop port=5432 user=score password=Rysherat2") as conn:
+        with conn.cursor() as cursor:
+            nearest_unconfirmed_order_date = fetch_nearest_unconfirmed_order_date(cursor)
+            count_unconfirmed_orders = fetch_count_unconfirmed_orders(cursor)
+            count_orders_processed_today = fetch_count_orders_processed_today(cursor)
+
+    order_waiting_time = get_delta_from_now_in_sec(nearest_unconfirmed_order_date)
+    
+    return jsonify({'order_waiting_time': order_waiting_time,
+                    'count_unconfirmed_orders': count_unconfirmed_orders,
+                    'count_orders_processed_today': count_orders_processed_today 
+                    })
+
+
+def get_delta_from_now_in_sec(date):
+        if date:
+            time_delta = datetime.datetime.now() - date
+            return time_delta.seconds
+        return 0        
+
+
+def fetch_nearest_unconfirmed_order_date(cursor):
+    cursor.execute("SELECT created FROM orders WHERE confirmed IS NULL ORDER BY created DESC LIMIT 1")
+    nearest_unconfirmed_order_date = cursor.fetchall()
+    if nearest_unconfirmed_order_date:
+        return nearest_unconfirmed_order_date[0][0]
+    return 0
+
+def fetch_count_unconfirmed_orders(cursor):
+    cursor.execute("SELECT COUNT(*) FROM orders WHERE confirmed IS NULL")
+    count_unconfirmed_orders = cursor.fetchall()
+    if count_unconfirmed_orders:
+        return count_unconfirmed_orders[0][0]
+    return 0
+
+
+def fetch_count_orders_processed_today(cursor):
+    cursor.execute("SELECT COUNT(*) FROM orders WHERE confirmed IS NOT NULL AND created >= 'today'")
+    count_orders_processed_today = cursor.fetchall()
+    if count_orders_processed_today:
+        return count_orders_processed_today[0][0]
+    return 0
+
+
+def fetch_KPI(conn):
+    with conn.cursor() as cursor:
+        nearest_unconfirmed_order_date = fetch_nearest_unconfirmed_order_date(cursor)
+        count_unconfirmed_orders = fetch_count_unconfirmed_orders(cursor)
+        count_orders_processed_today = fetch_count_orders_processed_today(cursor)
+
 
 if __name__ == "__main__":
     app.run()
+
